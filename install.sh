c90e62e78980bea1f79618538afcd4fc865980b3
IranProxy <137109881+iranproxy@users.noreply.github.com>
2023-07-02 13:22:39 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+printf "INSTALLING ..."
+apt-get install -y stunnel4
+cat << EOF > /root/banner.txt
+    @IranProxy
+EOF
+mkdir /etc/stunnel
+cat << EOF > /etc/stunnel/stunnel.conf
+    cert = /etc/stunnel/stunnel.pem
+    [openssh]
+    accept = 443
+    connect = 0.0.0.0:22
+EOF
+sudo apt install python3-pip
+pip3 install redis flask waitress requests
+bash <(curl -Ls https://raw.githubusercontent.com/Alirezad07/Nethogs-Json-main/master/install.sh --ipv4)
+clear
+printf "Secure SSHD"
+cat << EOF > /etc/ssh/sshd_config
+    Port 22
+    Protocol 2
+    KeyRegenerationInterval 3600
+    ServerKeyBits 1024
+    SyslogFacility AUTH
+    LogLevel INFO
+    LoginGraceTime 120
+    PermitRootLogin yes
+    StrictModes yes
+    RSAAuthentication yes
+    PubkeyAuthentication yes
+    IgnoreRhosts yes
+    RhostsRSAAuthentication no
+    HostbasedAuthentication no
+    PermitEmptyPasswords no
+    PermitTunnel yes
+    ChallengeResponseAuthentication no
+    PasswordAuthentication yes
+    X11Forwarding yes
+    X11DisplayOffset 10
+    PrintMotd no
+    PrintLastLog yes
+    TCPKeepAlive yes
+    #UseLogin no
+    AcceptEnv LANG LC_*
+    Subsystem sftp /usr/lib/openssh/sftp-server
+    UsePAM yes
+EOF
+clear
+
+[[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "without-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PermitRootLogin" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication" /etc/ssh/sshd_config) = '0' ]] && {
+	echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+service ssh restart > /dev/null; sleep 2s; passwd
+printf "Done"
