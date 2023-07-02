a3b2991f15d5605d284b0e509b1b2c43ef099bc6
IranProxy <137109881+iranproxy@users.noreply.github.com>
2023-07-02 13:27:26 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,48 +3,49 @@ printf "INSTALLING ..."
 apt update
 apt-get install -y stunnel4
 cat << EOF > /root/banner.txt
-    @IranProxy
+@IranProxy
 EOF
 mkdir /etc/stunnel
 cat << EOF > /etc/stunnel/stunnel.conf
-    cert = /etc/stunnel/stunnel.pem
-    [openssh]
-    accept = 443
-    connect = 0.0.0.0:22
+cert = /etc/stunnel/stunnel.pem
+[openssh]
+accept = 443
+connect = 0.0.0.0:22
 EOF
 sudo apt install python3-pip -y
+sudo apt install redis -y
 pip3 install redis flask waitress requests
 bash <(curl -Ls https://raw.githubusercontent.com/Alirezad07/Nethogs-Json-main/master/install.sh --ipv4)
 clear
 printf "Secure SSHD"
 cat << EOF > /etc/ssh/sshd_config
-    Port 22
-    Protocol 2
-    KeyRegenerationInterval 3600
-    ServerKeyBits 1024
-    SyslogFacility AUTH
-    LogLevel INFO
-    LoginGraceTime 120
-    PermitRootLogin yes
-    StrictModes yes
-    RSAAuthentication yes
-    PubkeyAuthentication yes
-    IgnoreRhosts yes
-    RhostsRSAAuthentication no
-    HostbasedAuthentication no
-    PermitEmptyPasswords no
-    PermitTunnel yes
-    ChallengeResponseAuthentication no
-    PasswordAuthentication yes
-    X11Forwarding yes
-    X11DisplayOffset 10
-    PrintMotd no
-    PrintLastLog yes
-    TCPKeepAlive yes
-    #UseLogin no
-    AcceptEnv LANG LC_*
-    Subsystem sftp /usr/lib/openssh/sftp-server
-    UsePAM yes
+Port 22
+Protocol 2
+KeyRegenerationInterval 3600
+ServerKeyBits 1024
+SyslogFacility AUTH
+LogLevel INFO
+LoginGraceTime 120
+PermitRootLogin yes
+StrictModes yes
+RSAAuthentication yes
+PubkeyAuthentication yes
+IgnoreRhosts yes
+RhostsRSAAuthentication no
+HostbasedAuthentication no
+PermitEmptyPasswords no
+PermitTunnel yes
+ChallengeResponseAuthentication no
+PasswordAuthentication yes
+X11Forwarding yes
+X11DisplayOffset 10
+PrintMotd no
+PrintLastLog yes
+TCPKeepAlive yes
+#UseLogin no
+AcceptEnv LANG LC_*
+Subsystem sftp /usr/lib/openssh/sftp-server
+UsePAM yes
 EOF
 clear
 
